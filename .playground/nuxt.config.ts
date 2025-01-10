b035832ebea8ad8f935c2681cb590cfd54d7857d
ManUtopiK <emmanuel.salomon@gmail.com>
2025-01-10 03:26:35 +0100

    chore: added nuxt compatibility date

--- .playground/nuxt.config.ts
+++ .playground/nuxt.config.ts
@@ -3,10 +3,13 @@ import { fileURLToPath } from 'node:url'
 export default defineNuxtConfig({
   extends: ['..'],
   modules: ['@nuxt/eslint'],
+
   eslint: {
     config: {
       // Use the generated ESLint config for lint root project as well
       rootDir: fileURLToPath(new URL('..', import.meta.url))
     }
-  }
+  },
+
+  compatibilityDate: '2025-01-10'
 })
