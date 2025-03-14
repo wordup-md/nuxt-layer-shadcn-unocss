c7a22669e3ff621587108a479a4fac93a18e65c6
ManUtopiK <emmanuel.salomon@gmail.com>
2025-03-14 23:02:51 +0100

    feat: set scroll area in search dialog

--- components/layout/SearchDialog.vue
+++ components/layout/SearchDialog.vue
@@ -18,133 +18,135 @@
           @keydown.down="handleNavigate(1)"
           @keydown.up="handleNavigate(-1)"
         />
-        <UiCommandList
-          class="text-sm"
-          @escape-key-down="open = false"
-        >
-          <template v-if="!input?.length">
-            <template
-              v-for="item in navigation"
-              :key="item._path"
-            >
+        <UiScrollArea orientation="vertical">
+          <UiCommandList
+            class="text-sm"
+            @escape-key-down="open = false"
+          >
+            <template v-if="!input?.length">
+              <template
+                v-for="item in navigation"
+                :key="item._path"
+              >
+                <UiCommandGroup
+                  v-if="item.children"
+                  :heading="item.title"
+                  class="p-1.5"
+                >
+                  <NuxtLink
+                    v-for="child in item.children"
+                    :key="child.id"
+                    :to="child._path"
+                  >
+                    <UiCommandItem :value="child._path">
+                      <SmartIcon
+                        v-if="child.icon"
+                        :name="child.icon"
+                        class="mr-2 size-4"
+                      />
+                      <div
+                        v-else
+                        class="mr-2 size-4"
+                      />
+                      <span>{{ child.title }}</span>
+                    </UiCommandItem>
+                  </NuxtLink>
+                </UiCommandGroup>
+                <UiCommandSeparator v-if="item.children" />
+              </template>
               <UiCommandGroup
-                v-if="item.children"
-                :heading="item.title"
+                v-if="darkModeToggle"
+                heading="Theme"
                 class="p-1.5"
               >
-                <NuxtLink
-                  v-for="child in item.children"
-                  :key="child.id"
-                  :to="child._path"
+                <UiCommandItem
+                  value="light"
+                  @click="colorMode.preference = 'light'"
                 >
-                  <UiCommandItem :value="child._path">
-                    <SmartIcon
-                      v-if="child.icon"
-                      :name="child.icon"
-                      class="mr-2 size-4"
-                    />
-                    <div
-                      v-else
-                      class="mr-2 size-4"
-                    />
-                    <span>{{ child.title }}</span>
-                  </UiCommandItem>
-                </NuxtLink>
+                  <Icon
+                    name="lucide:sun"
+                    class="mr-2 size-4"
+                  />
+                  <span>Light</span>
+                </UiCommandItem>
+                <UiCommandItem
+                  value="dark"
+                  @click="colorMode.preference = 'dark'"
+                >
+                  <Icon
+                    name="lucide:moon"
+                    class="mr-2 size-4"
+                  />
+                  <span>Dark</span>
+                </UiCommandItem>
+                <UiCommandItem
+                  value="system"
+                  @click="colorMode.preference = 'auto'"
+                >
+                  <Icon
+                    name="lucide:monitor"
+                    class="mr-2 size-4"
+                  />
+                  <span>System</span>
+                </UiCommandItem>
               </UiCommandGroup>
-              <UiCommandSeparator v-if="item.children" />
             </template>
-            <UiCommandGroup
-              v-if="darkModeToggle"
-              heading="Theme"
+
+            <div
+              v-else-if="searchResult?.length"
               class="p-1.5"
             >
-              <UiCommandItem
-                value="light"
-                @click="colorMode.preference = 'light'"
+              <NuxtLink
+                v-for="(item, i) in searchResult"
+                :id="i"
+                :key="item.id"
+                :to="item.id"
+                class="flex select-none rounded-md p-2 hover:cursor-pointer hover:bg-muted"
+                :class="[i === activeSelect && 'bg-muted']"
+                @click="
+                  open = false;
+                  activeSelect = i;
+                "
               >
-                <Icon
-                  name="lucide:sun"
-                  class="mr-2 size-4"
+                <SmartIcon
+                  v-if="getItemIcon(item.id)"
+                  :name="getItemIcon(item.id)"
+                  class="mr-2 size-4 shrink-0 self-center"
                 />
-                <span>Light</span>
-              </UiCommandItem>
-              <UiCommandItem
-                value="dark"
-                @click="colorMode.preference = 'dark'"
-              >
-                <Icon
-                  name="lucide:moon"
-                  class="mr-2 size-4"
+                <div
+                  v-else
+                  class="mr-2 size-4 shrink-0"
                 />
-                <span>Dark</span>
-              </UiCommandItem>
-              <UiCommandItem
-                value="system"
-                @click="colorMode.preference = 'auto'"
-              >
-                <Icon
-                  name="lucide:monitor"
-                  class="mr-2 size-4"
-                />
-                <span>System</span>
-              </UiCommandItem>
-            </UiCommandGroup>
-          </template>
 
-          <div
-            v-else-if="searchResult?.length"
-            class="p-1.5"
-          >
-            <NuxtLink
-              v-for="(item, i) in searchResult"
-              :id="i"
-              :key="item.id"
-              :to="item.id"
-              class="flex select-none rounded-md p-2 hover:cursor-pointer hover:bg-muted"
-              :class="[i === activeSelect && 'bg-muted']"
-              @click="
-                open = false;
-                activeSelect = i;
-              "
+                <span
+                  v-for="(subtitle, j) in item.titles"
+                  :key="`${subtitle}${j}`"
+                  class="flex shrink-0 self-center"
+                >
+                  {{ subtitle }}
+                  <Icon
+                    name="lucide:chevron-right"
+                    class="mx-0.5 self-center text-muted-foreground"
+                  />
+                </span>
+                <span class="shrink-0 self-center">
+                  {{ item.title }}
+                </span>
+                <span
+                  class="ml-2 self-center truncate text-xs text-muted-foreground"
+                  v-html="getHighlightedContent(item.content)"
+                />
+              </NuxtLink>
+            </div>
+
+            <div
+              v-else
+              class="pt-4 text-center text-muted-foreground"
             >
-              <SmartIcon
-                v-if="getItemIcon(item.id)"
-                :name="getItemIcon(item.id)"
-                class="mr-2 size-4 shrink-0 self-center"
-              />
-              <div
-                v-else
-                class="mr-2 size-4 shrink-0"
-              />
-
-              <span
-                v-for="(subtitle, j) in item.titles"
-                :key="`${subtitle}${j}`"
-                class="flex shrink-0 self-center"
-              >
-                {{ subtitle }}
-                <Icon
-                  name="lucide:chevron-right"
-                  class="mx-0.5 self-center text-muted-foreground"
-                />
-              </span>
-              <span class="shrink-0 self-center">
-                {{ item.title }}
-              </span>
-              <span
-                class="ml-2 self-center truncate text-xs text-muted-foreground"
-                v-html="getHighlightedContent(item.content)"
-              />
-            </NuxtLink>
-          </div>
-
-          <div
-            v-else
-            class="pt-4 text-center text-muted-foreground"
-          >
-            No results found.
-          </div>
-        </UiCommandList>
+              No results found.
+            </div>
+          </UiCommandList>
+        </UiScrollArea>
       </UiCommand>
     </UiDialogContent>
   </UiDialog>
