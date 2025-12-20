4987557aaad12648279a6f1abf77638b168dad55
ManUtopiK <emmanuel.salomon@gmail.com>
2025-12-20 04:56:18 +0100

    fix: remove umami

--- plugins/umami.ts
+++ plugins/umami.ts
@@ -1,13 +1,13 @@
 export default defineNuxtPlugin(() => {
-  const { enable, dataWebsiteId, src } = useConfig().value.site.umami
+  // const { enable, dataWebsiteId, src } = useConfig().value.site.umami
 
-  if (enable && !import.meta.dev) {
-    useScriptUmamiAnalytics({
-      scriptInput: {
-        src,
-        defer: true,
-      },
-      websiteId: dataWebsiteId,
-    })
-  }
+  // if (enable && !import.meta.dev) {
+  //   useScriptUmamiAnalytics({
+  //     scriptInput: {
+  //       src,
+  //       defer: true,
+  //     },
+  //     websiteId: dataWebsiteId,
+  //   })
+  // }
 })
